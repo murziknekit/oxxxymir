9e1bcae6ba364c4f1a8b21d5bad4477dbde4b4ed
murziknekit <152699704+murziknekit@users.noreply.github.com>
2023-12-02 16:07:45 +0300

    jo

---
+++ comedy.py
@@ -0,0 +1,12 @@
+def hg(x,y):
+    if x < 0  and y < 0:
+        print('|')
+    elif x > 0 and y > 0:
+        print('|||')
+    elif x > 0 and y > 0:
+        print('||')
+    else :
+        print('|\/')
+hg(5,-5)
+
+        
